2c80afedcd1a511cd91f6871790b5018a1538066
codeBrian <89549557+code-Brian@users.noreply.github.com.>
2024-01-29 21:28:47 -0800

    figured out how to transmit data between forms

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -10,9 +10,21 @@ public class MainController : Controller
         return View();
     }
 
-    [HttpGet("DojoSurvey")]
-    public ViewResult DojoSurvey()
+    [HttpGet("results/{text}/{number}")]
+    public ViewResult Results(string text, int number)
     {
+        ViewBag.Text = text;
+        ViewBag.Number = number;
+        Console.WriteLine(text);
+        Console.WriteLine(number);
         return View();
+    }
+
+    [HttpPost("dojoForm")]
+    public RedirectToActionResult DojoForm(string Text, int Number)
+    { 
+        Console.WriteLine($"Text before redirect: {Text}");
+        Console.WriteLine($"Number before redirect: {Number}");
+        return RedirectToAction("Results", new {text = Text, number = Number});
     }
 }
